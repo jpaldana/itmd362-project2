ca07551742788e29b3ac394bec7f0dbdd92e5146
JP Aldana <aldana.jpg@gmail.com>
2018-03-25 20:15:33 -0500

    Add basic validator structure for payment form handler

--- js/site.js
+++ js/site.js
@@ -1,7 +1,23 @@
 $(function() {
+  var paymentName = $("#payment_name");
+  var paymentEmail = $("#payment_email");
+  var paymentZip = $("#payment_zip");
+  var paymentCc = $("#payment_cc");
+  var paymentDate = $("#payment_date");
+  var paymentCvv = $("#payment_cvv");
+
+  var validatePaymentFields = function() {
+
+  };
+
   var runPaymentFlow = function(e) {
     e.preventDefault();
-
+    if (validatePaymentFields()) {
+      console.log("Success, pretend to POST data request or something.");
+    }
+    else {
+      console.log("Failed. Show an error.");
+    }
   };
 
   $("#payment_checkout").on("click", runPaymentFlow);
