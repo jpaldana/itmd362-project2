b2296c71fc9eaf5bd82b85e8cca061ecf69a5e67
Randal <rkwok1@hawk.iit.edu>
2018-03-25 21:38:22 -0500

    Add checks for e-mail, date, ccv

--- js/site.js
+++ js/site.js
@@ -1,12 +1,15 @@
 $(function() {
   var paymentName = $("#payment_name");
   var paymentEmail = $("#payment_email");
+  var emailRegex = /^[^\s@]+@[^\s@]+$/;
   var paymentZip = $("#payment_zip");
   var zipRegex = /^\d{5}$/;
   var paymentCc = $("#payment_cc");
   var ccRegex = /^\d{15,16}$/;
   var paymentDate = $("#payment_date");
+  var dateRegex = /^\d{2}\/\d{2}$/;
   var paymentCvv = $("#payment_cvv");
+  var ccvRegex = /^\d{3,4}$/;
   
   var paymentLog = $("#payment_log");
 
@@ -30,6 +33,10 @@ $(function() {
       logEvent("Empty field detected.");
       isValid = false;
     }
+    if (!emailRegex.test(payment_email.val())) {
+      isValid = false;
+      logEvent("Invalid e-mail");
+    }
     if (!zipRegex.test(paymentZip.val())) {
       isValid = false;
       logEvent("Invalid zipcode");
@@ -37,6 +44,14 @@ $(function() {
     if (!ccRegex.test(paymentCc.val())) {
       isValid = false;
       logEvent("Invalid Credit Card Number");
+    }
+    if (!dateRegex.test(paymentDate.val())) {
+      isValid = false;
+      logEvent("Invalid Expiration Date");
+    }
+    if (!ccvRegex.test(paymentCvv.val())) {
+      isValid = false;
+      logEvent("Invalid CCV");
     }
 
     return isValid;
