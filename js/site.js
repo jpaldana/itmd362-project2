476e208b5d12201223de2ac391555664edbccbd3
JP Aldana <aldana.jpg@gmail.com>
2018-04-12 13:49:01 -0500

    Change sprintf call to a string prototype instead

--- js/site.js
+++ js/site.js
@@ -296,12 +296,12 @@ $(function() {
 
   // Add a sprintf()-like JS call
   // https://stackoverflow.com/questions/610406/javascript-equivalent-to-printf-string-format
-  if (!String.format) {
-    String.format = function(format) {
-      var args = Array.prototype.slice.call(arguments, 1);
-      return format.replace(/{(\d+)}/g, function(match, number) { 
+  if (!String.prototype.format) {
+    String.prototype.format = function() {
+      var args = arguments;
+      return this.replace(/{(\d+)}/g, function(match, number) { 
         return typeof args[number] !== 'undefined'
-          ? args[number] 
+          ? args[number]
           : match
         ;
       });
