0e19fdd74bdc2fb66674eb9fc96fe210817acc93
JP Aldana <aldana.jpg@gmail.com>
2018-04-12 13:47:27 -0500

    Add sprintf-like JS call

--- js/site.js
+++ js/site.js
@@ -293,4 +293,18 @@ $(function() {
       init();
     });
   }
+
+  // Add a sprintf()-like JS call
+  // https://stackoverflow.com/questions/610406/javascript-equivalent-to-printf-string-format
+  if (!String.format) {
+    String.format = function(format) {
+      var args = Array.prototype.slice.call(arguments, 1);
+      return format.replace(/{(\d+)}/g, function(match, number) { 
+        return typeof args[number] !== 'undefined'
+          ? args[number] 
+          : match
+        ;
+      });
+    };
+  }
 });
